54874f79616300c104c6de51f757120ce515b455
isaskou <isabel.skou@yahoo.fr>
2021-06-21 15:57:40 +0200

    End of section 2

---
+++ DatingApp.API/Controllers/Users.cs
@@ -0,0 +1,36 @@
+﻿using DatingApp.DAL;
+using DatingApp.DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ DatingApp.DAL/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using DatingApp.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.DAL
+{
+    public class DataContext : DbContext
+    {
+        private readonly string _connString;
+        public DataContext()
+        {
+            _connString = @"server=LAPTOP-03RLPA2O\SQLEXPRESS;Database=DatingApp; Integrated Security=True;";
+        }
+
+        public DataContext(string connString)
+        {
+            _connString = connString;
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connString);
+        }
+
+    }
+}
